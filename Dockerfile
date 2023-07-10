adba2e05b2af077780f676d08d0bb9a793af99c2
Brekke-Green <brekke.andrew.green@gmail.com>
2023-07-10 12:45:26 -0400

    Adjust files for running flask app in docker container

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,8 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
-CMD [ "python", "./app.py" ]
+EXPOSE 5000
+
+ENTRYPOINT [ "python" ]
+
+CMD ["app.py"]
--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template
+import os
 from time import  time
 
 app = Flask(__name__)
@@ -6,6 +7,12 @@ app = Flask(__name__)
 plants = {"alocasia": ["2023/07/08/-08:00:00"]}
 
 @app.route("/")
-def hello_world():
-    return f"ENJOY SOME GOOD HYDRATIONS \n {plants}"
+def home():
+    return render_template('index.html', plants=plants)
+
+##    return f"ENJOY SOME GOOD HYDRATIONS \n {plants}"
+
+if  __name__ == "__main__":
+    port= int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
 
