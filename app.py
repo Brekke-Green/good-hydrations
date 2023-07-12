e867d16b7d5f913b63c44b433256f39ce3ba33e2
Brekke-Green <brekke.andrew.green@gmail.com>
2023-07-12 13:41:11 -0400

    Fix formatting

--- app.py
+++ app.py
@@ -15,6 +15,6 @@ def signup():
     return render_template('signup.html')
 
 if  __name__ == "__main__":
-    port= int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5000))
     app.run(debug=False, host='0.0.0.0', port=port)
 
