e341d9e8ab9765b636eee379d9da088705c302ef
Brekke-Green <brekke.andrew.green@gmail.com>
2023-08-08 13:16:02 -0400

    Update header name for rendering single plant log

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from time import  time
 app = Flask(__name__)
 
 plants_data = {
-        "alocasia": ["2023/07/08/-08:00:00", "2023/07/20/-08:00:00", "2023/08/3/-08:00:00"],
-        "hedgehog aloe": ["2023/07/12/-08:00:00", "2023/07/18/-08:00:00"],
+        "alocasia": ["2023/07/08/-08:00:00", "2023/07/20/-08:00:00", "2023/08/03/-08:00:00"],
+        "hedgehog_aloe": ["2023/07/12/-08:00:00", "2023/07/18/-08:00:00"],
         "pothos": ["2023/07/12/-08:00:00", "2023/07/14/-08:00:00", "2023/07/20/-08:00:00"],
         "anthurium": ["2023/08/06/-08:00:00"],
         }
@@ -30,7 +30,8 @@ def login():
 @app.route("/plants")
 def plants():
     plant_urls = generate_urls(plants_data)
-    return render_template('plants.html', plants=plants_data, plant_urls=plant_urls)
+    plants_length = len(plants_data.keys())
+    return render_template('plants.html', plants=plants_data, plant_urls=plant_urls, plants_length=plants_length)
 
 def generate_urls(plants_data):
     result = {}
@@ -38,14 +39,11 @@ def generate_urls(plants_data):
         result[key] = f"/plants/{key}"
     return result
 
-####NOT FUNCTIONAL - continue to build out selecting individual plant and reporting logs/data
 @app.route("/plants/<name>")
 def plant(name):
-##    plant_logs = plants_data[name] 
-##    return render_template('plants.html', plant=name, logs=plant_logs)
     plant_log = {name: plants_data[name]}
     plant_urls = generate_urls(plant_log)
-    return render_template('plants.html', plants=plant_log, plant_urls=plant_urls)
+    return render_template('plants.html', plants=plant_log, plant_urls=plant_urls, plants_length=1)
 
 if  __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
